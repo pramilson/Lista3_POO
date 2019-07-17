4a3adb205051d59ed65bce1f90ff8055d19379f3
pramilson <ramilson_silva@id.uff.br>
2019-07-17 00:06:49 -0300

    diferença entre erro e exceção

--- Exercicio4/Main.java
+++ Exercicio4/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
         Televisao TV = new Televisao();
+        Televisao TV2 = new Televisao();
         System.out.println(TV.consultarCanal);
         System.out.println(TV.consultarVolume);
     }
